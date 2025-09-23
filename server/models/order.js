358522219b04c297fdc0beff6e4151bb2d466540
Khoaitk5 <dangkhoa.itk5@gmail.com>
2025-09-23 20:59:20 +0700

    models by khoa

---
+++ server/models/order.js
@@ -0,0 +1,27 @@
+// models/orderModel.js
+
+class Order {
+  constructor({
+    order_id,
+    user_id,
+    shop_id,
+    shipper_id,
+    total_price,
+    status,
+    payment_method,
+    created_at,
+    updated_at,
+  }) {
+    this.order_id = order_id;
+    this.user_id = user_id;
+    this.shop_id = shop_id;
+    this.shipper_id = shipper_id;
+    this.total_price = total_price;
+    this.status = status;
+    this.payment_method = payment_method;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+  }
+}
+
+module.exports = Order;
---
+++ server/models/video_comment.js
@@ -0,0 +1,13 @@
+// models/videoCommentModel.js
+
+class VideoComment {
+  constructor({ comment_id, video_id, user_id, content, created_at }) {
+    this.comment_id = comment_id;
+    this.video_id = video_id;
+    this.user_id = user_id;
+    this.content = content;
+    this.created_at = created_at;
+  }
+}
+
+module.exports = VideoComment;
